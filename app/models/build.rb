b975f76501eabde567e4332de925c48200ec1c9b
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-03-12 15:22:46 -0400

    Creating controller and views for calc page, setting routes and building out Javascript math functionality

--- app/models/build.rb
+++ app/models/build.rb
@@ -3,11 +3,11 @@ class Build < ActiveRecord::Base
 	#validates :erd, :flange_diameter_left, :flange_diameter_right, :flange_center_right, :flange_center_left
 
 	def spoke_length_right
-		spoke_length(flange_radius_right, flange_center_right)
+		spoke_length(flange_radius_right, flange_center_right).round(2)
 	end
 
 	def spoke_length_left
-		spoke_length(flange_radius_left, flange_center_left)
+		spoke_length(flange_radius_left, flange_center_left).round(2)
 	end
 
 	def rim_radius
@@ -23,7 +23,7 @@ class Build < ActiveRecord::Base
 	end
 
 	def spoke_length(flange_radius, flange_center)
-		Math.sqrt(rim_radius**2 + flange_radius**2 + flange_center**2 - 2*rim_radius * flange_radius * Math.cos(720*spoke_crosses*Math::PI / 180*spoke_count)) - spoke_hole_diameter / 2
+		Math.sqrt(rim_radius ** 2 + flange_radius ** 2 + flange_center ** 2 - 2 * rim_radius * flange_radius * Math.cos(720 * spoke_crosses * Math::PI / 180 * spoke_count)) - (spoke_hole_diameter / 2)
 	end
 
 end
