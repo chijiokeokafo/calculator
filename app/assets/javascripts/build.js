c761dd5c1ed3b5f679526668e55d84ef0e3d894e
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-03-15 23:53:24 -0400

    Adding styling to build show view and jQuery effects to build.js

--- app/assets/javascripts/build.js
+++ app/assets/javascripts/build.js
@@ -6,3 +6,10 @@ $(document).on('ready page:load', function(){
 		$.getScript('/builds?search=' + searchValue);
 	});
 });
+
+$(document).on('ready page:load', function(){
+	$( '#specs' ).slideDown(2500).css('opacity', 0.3);
+	$('#specs').mouseenter(function(){
+		$(this).fadeTo('slow', 1);
+	});
+});
