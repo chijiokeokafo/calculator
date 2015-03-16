4904808d8a8b84f201e87bf3247e84d4602fa4ee
Jenia Tretiyakova <zhanyat@gmail.com>
2015-03-16 17:43:59 -0400

    Little bit of a tidy.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,9 @@
 class LikesController < ApplicationController
 
   def create
-    # binding.pry
     @like = Like.find_or_create_by(user_id: current_user.id, photo_id: params[:photo])
     @like.save
 
-    # if @like.save
-    # end
-
-    # respond_to do |format|
-    #   format.html
-    #   format.js
-    # end
     redirect_to photo_path(params[:photo])
   end
 
