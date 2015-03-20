a7e5b477ac3361dc18a80df78841ced2d3835b7e
Jenia Tretiyakova <zhanyat@gmail.com>
2015-03-20 13:17:48 -0400

    added some changes to animation

--- app/assets/javascripts/wheel.js
+++ app/assets/javascripts/wheel.js
@@ -1,7 +1,7 @@
 
 $(document).on('ready page:load', function() {
 	$(".wheel-trigger").on("click", function(e) {
-		e.preventDefault();
+		// e.preventDefault();
 
 		var self = $(this);
 		$(".img-container").toggleClass('is-rolling');
