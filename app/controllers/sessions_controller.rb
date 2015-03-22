2e4c874150f8c6e2048113c6cf411807afd67b2f
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-03-22 00:27:08 -0400

    modifying sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,20 @@ class SessionsController < ApplicationController
 	skip_before_filter :require_login, except: [:destroy]
 	
 
-	def index
-		render 
-	end
+	# def index
+	# 	render 
+	# end
 
-	def new
-		render text: 
-	end
+	# def new
+	# 	render text: 
+	# end
 
-	def create
-		render text: 
-	end
+	# def create
+	# 	render text: 
+	# end
 
-	def destroy
-		render text: 
-	end
+	# def destroy
+	# 	render text: 
+	# end
 
 end
