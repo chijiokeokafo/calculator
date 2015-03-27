261f8cc85ddf40133ee4e6ee97715ad7cf2ba979
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-03-26 20:04:08 -0400

    Modifying user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,12 @@ class User < ActiveRecord::Base
 
   has_many :likes
 
-  authenticates_with_sorcery!
+  authenticates_with_sorcery! do |config|
+    config.authentications_class = Authentication 
+  end
+
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 6 }
   validates :password, confirmation: true, on: :create
